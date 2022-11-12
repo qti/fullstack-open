a9231de37704469e4cfa008d3a94ab7473bdb333
jordananslow <jordananslow@gmail.com>
2022-11-12 11:29:56 +1100

    Exercises 2.1.-2.5

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,19 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+const Part = ({ part }) => <p>
+  {part.name} {part.exercises}
+</p>;
+const Content = ({ parts }) => <>
+  {parts.map((part) => <Part key={part.id} part={part} />
+  )}
+</>;
+export const Course = ({ course }) => {
+  const { name, parts } = course;
+  return (
+    <div>
+      <Header course={name} />
+      <Content parts={parts} />
+      <Total sum={parts.reduce((acc, { exercises }) => acc + exercises, 0)} />
+    </div>
+  );
+};
