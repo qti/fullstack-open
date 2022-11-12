de08fa74c0009721537115cac6292a9b3557159e
jordananslow <jordananslow@gmail.com>
2022-11-12 11:32:09 +1100

    Reformatting

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,12 +1,16 @@
 const Header = ({ course }) => <h1>{course}</h1>;
+
 const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
 const Part = ({ part }) => <p>
   {part.name} {part.exercises}
 </p>;
+
 const Content = ({ parts }) => <>
   {parts.map((part) => <Part key={part.id} part={part} />
   )}
 </>;
+
 export const Course = ({ course }) => {
   const { name, parts } = course;
   return (
