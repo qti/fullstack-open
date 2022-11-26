e68b8bc34b34c629ccd6775b9bbfb88dc98800d8
jordananslow <jordananslow@gmail.com>
2022-11-26 14:06:58 +1100

    Exercises 4.15.-4.23.

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-require('express-async-errors')
 const config = require('./utils/config')
-const blogRouter = require('./controller/blogs')
+
+require('express-async-errors')
+
+const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI)
 
@@ -12,5 +16,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
